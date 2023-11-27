cbbd3d6cec01612493202a3894f1111b73b2ca23
Matt <mattiregojos@gmail.com>
2023-11-27 08:41:02 +0000

    Add and modify files

--- git-push.sh
+++ git-push.sh
@@ -2,6 +2,18 @@
 # sh git-push.sh
 # https://github.com/mregojos/CI-CD-GCP
 
+# For Security
+# Cleanup environment variables
+rm -f ./.env.*
+rm -f ./*/.env.*
+rm -f ./*/*/.env.*
+rm -f ./*/*/*/.env.*
+rm -f ./*/*/*/*/.env.*
+rm -rf .ipynb_checkpoints
+rm -rf ./*/.ipynb_checkpoints
+rm -rf ./*/*/.ipynb_checkpoints
+echo "Successfully removed .env.* and checkpoints files ready to be pushed to repository."
+
 git add .
 # git config --global user.email "<EMAIL_ADDRESS>"
 # git config --global user.email ""
