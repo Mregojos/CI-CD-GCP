8407e92d1fe9e65421978a333e15b6e298748721
Matt <mattiregojos@gmail.com>
2023-11-27 08:53:35 +0000

    Add and modify files

--- synch.sh
+++ synch.sh
@@ -3,6 +3,6 @@
 cp -rf app-deployment/Main.py ~/ci-cd-gcp/app
 cp -rf app-deployment/pages/Agent.py ~/ci-cd-gcp/pages
 
-# Infrastrcuture Automation GCP Repo
+# Infrastructure Automation GCP Repo
 cp -rf infrastructure-automation-gcp.sh ~/ci-cd-gcp
 cp -rf cleanup.sh ~/ci-cd-gcp
