f9a5fa1e554a358be655e95b394af076ba448452
Matt <mattiregojos@gmail.com>
2023-11-29 07:12:22 +0000

    Add and modify files

--- update-app.sh
+++ update-app.sh
@@ -1,7 +1,7 @@
 # Model Deployment Repo
 # cp -rf app-deployment/* ~/ci-cd-gcp/app
 cp -rf app-deployment/Main.py ~/ci-cd-gcp/app
-cp -rf app-deployment/pages/Agent.py ~/ci-cd-gcp/pages
+cp -rf app-deployment/pages/Agent.py ~/ci-cd-gcp/app/pages
 
 # Infrastructure Automation GCP Repo
 # cp -rf infrastructure-automation-gcp.sh ~/ci-cd-gcp
