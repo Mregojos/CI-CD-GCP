837c76148b3a40d1831d0a63d1b30a2aa297b25d
Matt <mattiregojos@gmail.com>
2023-11-30 07:40:58 +0000

    Add and modify files

--- configuration-management-open-source-tool/cm-gcp.sh
+++ configuration-management-open-source-tool/cm-gcp.sh
@@ -1,11 +1,25 @@
 # Configuration Management on GCP
 
+# Environemnt Variable
+ZONE="us-west1-a"
+
 # Configuration Management
 sh cm.sh
 
 # Try localhost
 # ansible-playbook <playbook name>.yaml
 ansible-playbook playbooks/localhost.yaml
+
+# Create a ssh key
+ssh-keygen
+cat ~/.ssh/id_rsa.pub
+
+# Create two Compute Engine instances
+gcloud compute instances create vm-a vm-b --zone=$ZONE
+
+# Copy the key to other machines
+mkdir ~/.ssh
+nano ~/..sh/authorized_keys
 
 # With inventory
 # ansible-playbook <playbook name>.yaml -v <inventory>.yaml
