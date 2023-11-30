df7644883bc3f53e455d1684f383a163d16173d9
Matt <mattiregojos@gmail.com>
2023-11-30 07:19:10 +0000

    Add and modify files

--- configuration-management-open-source-tool/cm-gcp.sh
+++ configuration-management-open-source-tool/cm-gcp.sh
@@ -4,6 +4,9 @@
 sh cm.sh
 
 # Try localhost
+# ansible-playbook <playbook name>.yaml
 ansible-playbook playbooks/localhost.yaml
-# or
-ansible-playbook localhost.yaml
+
+# With inventory
+# ansible-playbook <playbook name>.yaml -v <inventory>.yaml
+ansible-playbook <playbook name>.yaml -v <inventory>.yaml
