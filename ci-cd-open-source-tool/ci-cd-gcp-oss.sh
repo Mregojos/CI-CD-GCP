2b0d0d3b92f6f1d73e9419e10bb6369e9737047e
Matt <mattiregojos@gmail.com>
2023-12-01 10:48:09 +0000

    Add and modify files

--- ci-cd-open-source-tool/ci-cd-gcp-oss.sh
+++ ci-cd-open-source-tool/ci-cd-gcp-oss.sh
@@ -9,6 +9,39 @@ gcloud compute --project=$(gcloud config get project) firewall-rules create $FIR
 # password
 docker exec <CONTAINER_ID> cat /var/jenkins_home/secrets/initialAdminPassword
 
+# App Env
+cd app
+# DB_INSTANCE_NAME Address / Host
+DB_HOST=$(gcloud compute instances list --filter="name=$DB_INSTANCE_NAME" --format="value(networkInterfaces[0].accessConfigs[0].natIP)") 
+
+# Environment Variables for the app
+echo """
+DB_NAME:
+    '$DB_NAME'
+DB_USER:
+    '$DB_USER'
+DB_HOST:
+    '$DB_HOST'
+DB_PORT:
+    '$DB_PORT'
+DB_PASSWORD:
+    '$DB_PASSWORD'
+PROJECT_NAME:
+    '$PROJECT_NAME'
+ADMIN_PASSWORD:
+    '$ADMIN_PASSWORD'
+APP_PORT:
+    '$APP_PORT'
+APP_ADDRESS:
+    '$APP_ADDRESS'
+DOMAIN_NAME:
+    '$DOMAIN_NAME'
+SPECIAL_NAME:
+    '$SPECIAL_NAME'
+""" > .env.yaml
+
+cd ..
+
 # Example Pipeline
 pipeline {
     agent any
@@ -45,15 +78,22 @@ pipeline {
     }
 }
 
+#-------------------------
 pipeline {
     agent any
     stages {
+        stage ('Clone repository') {
+            steps {
+                sh """
+                https://github.com/mregojos/ci-cd-gcp
+                cd ci-cd-open-source-tool/app
+                """
+            }
+        }    
         stage ('Build') {
             steps {
                 sh """
-                  gcloud builds submit \
-                      --region=$CLOUD_BUILD_REGION \
-                      --tag $REGION-docker.pkg.dev/$(gcloud config get-value project)/$APP_ARTIFACT_NAME/$APP_NAME:$APP_VERSION
+                gcloud builds submit --region=us-west2 --tag us-west1-docker.pkg.dev/matt---project/ci-cd-gcp-oss-i-artifact-registry/ci-cd-gcp-oss-i:latest
                 """
             }
         }    
@@ -67,13 +107,9 @@ pipeline {
         stage ('Deploy') {
             steps {
                 sh """
-                    gcloud run deploy $APP_NAME \
-                        --max-instances=$MAX_INSTANCES --min-instances=$MIN_INSTANCES --port=$APP_PORT \
-                        --env-vars-file=$APP_ENV_FILE \
-                        --image=$REGION-docker.pkg.dev/$(gcloud config get project)/$APP_ARTIFACT_NAME/$APP_NAME:$APP_VERSION \
-                        --allow-unauthenticated \
-                        --region=$REGION \
-                        --service-account=$APP_SERVICE_ACCOUNT_NAME@$(gcloud config get project).iam.gserviceaccount.com 
+                   gcloud run deploy ci-cd-gcp-oss-i     --max-instances=1 --min-instances=1 --port=9000     --env-vars-file=.env.yaml     \
+                       --image=us-west1-docker.pkg.dev/matt---project/ci-cd-gcp-oss-i-artifact-registry/ci-cd-gcp-oss-i:latest     --allow-unauthenticated     \
+                       --region=us-west1     --service-account=ci-cd-gcp-oss-i-app-sa@matt---project.iam.gserviceaccount.com 
                 """
             }
         }
---
+++ ci-cd-open-source-tool/pipeline.sh
@@ -0,0 +1,14 @@
+cat > pipeline.txt << EOF
+
+gcloud builds submit \
+    --region=$CLOUD_BUILD_REGION \
+    --tag $REGION-docker.pkg.dev/$(gcloud config get-value project)/$APP_ARTIFACT_NAME/$APP_NAME:$APP_VERSION
+  
+gcloud run deploy $APP_NAME \
+    --max-instances=$MAX_INSTANCES --min-instances=$MIN_INSTANCES --port=$APP_PORT \
+    --env-vars-file=$APP_ENV_FILE \
+    --image=$REGION-docker.pkg.dev/$(gcloud config get project)/$APP_ARTIFACT_NAME/$APP_NAME:$APP_VERSION \
+    --allow-unauthenticated \
+    --region=$REGION \
+    --service-account=$APP_SERVICE_ACCOUNT_NAME@$(gcloud config get project).iam.gserviceaccount.com 
+EOF
