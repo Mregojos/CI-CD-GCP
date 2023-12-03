7a1a43e3b17e6a6e4476d03f65785e181d61ca7a
Matt <mattiregojos@gmail.com>
2023-12-03 09:36:53 +0000

    Add and modify files

--- update-app.sh
+++ update-app.sh
@@ -4,7 +4,7 @@ cp -rf app-deployment/Main.py ~/ci-cd-gcp/app
 cp -rf app-deployment/pages/Agent.py ~/ci-cd-gcp/app/pages
 
 # ci-cd-gcp oss
-cp -rf app ci*tool
+cp -rf app ci-cd-oss-gcp
 
 # Infrastructure Automation GCP Repo
 # cp -rf infrastructure-automation-gcp.sh ~/ci-cd-gcp
