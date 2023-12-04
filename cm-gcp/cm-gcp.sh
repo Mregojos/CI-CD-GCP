4375f89cb95117437e9320e80c3a022fb2948620
Matt <mattiregojos@gmail.com>
2023-12-04 03:35:50 +0000

    Add and modify files

--- cm-gcp/cm-gcp.sh
+++ cm-gcp/cm-gcp.sh
@@ -27,28 +27,42 @@ sudo apt install ansible -y
 EOF
 
 # Create a Compute Engine instances
-gcloud compute instances create vm-c --zone=$ZONE \
+source cm-env.sh
+gcloud compute instances create vm-a vm-c --zone=$ZONE \
+    --metadata-from-file=startup-script=startup-script.sh
+    
+# Create a Compute Engine instances with ssh-keys
+source cm-env.sh
+gcloud compute instances create vm-b --zone=$ZONE \
     --metadata-from-file=startup-script=startup-script.sh \
-    --metadata=ssh-keys=$USER:$(cat ~/.ssh/id_rsa.pub)
+    --metadata=ssh-keys=$USER:"$(cat ~/.ssh/id_rsa.pub)"
+    
+# Create a Compute Engine instances adding SSH to vm a manually
+source cm-env.sh
+gcloud compute instances create vm-c --zone=$ZONE \
+    --metadata-from-file=startup-script=startup-script.sh
 
-# Use gcloud ssh to connect
+# Use gcloud ssh to connect vm-a
 gcloud compute ssh --zone $ZONE vm-a
 
 # Add ip adresses to inventory
-VM_A_IP=$(gcloud compute instances list --filter="name=vm-a" --format="value(networkInterfaces[0].accessConfigs[0].natIP)") 
+VM_A_IP=$(gcloud compute instances list --filter="name=vm-a" --format="value(networkInterfaces[0].accessConfigs[0].natIP)")
+VM_B_IP=$(gcloud compute instances list --filter="name=vm-b" --format="value(networkInterfaces[0].accessConfigs[0].natIP)")
 cat > inventory.txt <<EOF
 [vms]
 $VM_A_IP
+$VM_B_IP
 EOF
 
 # Copy Public Key
 cat ~/.ssh/id_rsa.pub
-# Copy the key to other machines
+# Copy the key to vm-a
 gcloud compute ssh --zone $ZONE vm-a
 nano ~/.ssh/authorized_keys
 
 # Test it
 ssh $USER@$VM_A_IP
+ssh $USER@$VM_B_IP
 
 # Test using ping
 ansible all -m ping -i inventory.txt -u $USER
