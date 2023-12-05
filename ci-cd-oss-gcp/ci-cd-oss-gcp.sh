070150445928262ac875b6629736c75e78df763f
Matt <mattiregojos@gmail.com>
2023-12-05 10:02:46 +0000

    Add and modify files

--- ci-cd-oss-gcp/ci-cd-oss-gcp.sh
+++ ci-cd-oss-gcp/ci-cd-oss-gcp.sh
@@ -8,6 +8,10 @@ gcloud compute --project=$(gcloud config get project) firewall-rules create $FIR
 # Get the password
 sudo docker exec jenkins-blueocean cat /var/jenkins_home/secrets/initialAdminPassword
 
+# Exec container
+sudo docker exec -it jenkins-blueocean sh
+cd /var/jenkins_home/workspace
+
 # Simple pipeline
 pipeline {
     agent any
@@ -48,7 +52,19 @@ pipeline {
         stage ('Clone the repo') {
             steps {
                 sh """
+                echo "Clone the reo"
+                
+                export NAME="MATT"
+                echo $NAME
+                
                 git clone https://github.com/mregojos/ci-cd-gcp
+                
+                ls
+                
+                cd ci-cd-oss-gcp
+                
+                ls
+                
                 
                 """
             }
@@ -56,14 +72,47 @@ pipeline {
         stage ('Build') {
             steps {
                 sh """
+                echo "Build"
                 
+                gcloud builds submit \
+                  --region=$CLOUD_BUILD_REGION \
+                  --tag $REGION-docker.pkg.dev/$(gcloud config get-value project)/$APP_ARTIFACT_NAME/$APP_NAME:$APP_VERSION
                 """
             }
         }
         stage ('Deploy') {
             steps {
                 sh """
-                ### Script
+                echo "Deploy"
+                """
+            }
+        }
+    }
+}
+
+pipeline {
+    agent any
+    stages {
+        stage ('Clone the repo') {
+            steps {
+                echo "Clone the repo"
+                sh """
+                NAME_ID="MATT"
+                export NAME_ID="MATT"
+                """
+            }
+        }    
+        stage ('Build') {
+            steps {
+                sh """
+                echo "Build"
+                """
+            }
+        }
+        stage ('Deploy') {
+            steps {
+                sh """
+                echo "Deploy"
                 """
             }
         }
